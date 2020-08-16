9e9c8c110f2796d42858e7e4b5e5dd57152154a4
马健 <jian.ma01@hand-china.com>
2020-08-16 23:38:52 +0800

    学习感想

--- Week_01/ContainerWithMostWater.java
+++ Week_01/ContainerWithMostWater.java
@@ -6,14 +6,30 @@
  * @createTime 2020年08月14日 14:35:00
  */
 public class ContainerWithMostWater {
+
+//    public int maxArea(int[] height) {
+//        int max = 0;
+//        for (int i = 0; i < height.length; i++) {
+//            for (int j = i + 1; j < height.length; j++) {
+//                int area = (j - i) * Math.min(height[i], height[j]);
+//                max = Math.max(area, max);
+//            }
+//        }
+//        return max;
+//    }
+
+
     public int maxArea(int[] height) {
         int max = 0;
-        for (int i = 0; i < height.length; i++) {
-            for (int j = i + 1; j < height.length; j++) {
-                int area = (j - i) * Math.min(height[i], height[j]);
-                max = Math.max(area, max);
-            }
+        for (int i = 0, j = height.length - 1; i != j; ) {
+            // 每次都移动高度较小的一端
+            int h = height[i] > height[j] ? height[j--] : height[i++];
+            max = Math.max(max, (j - i + 1) * h);
         }
         return max;
     }
+
+//    public int maxArea(int[] height) {
+//
+//    }
 }
