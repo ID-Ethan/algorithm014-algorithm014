35c694702f1ae4c6c892ef06afca96a30be86c25
马健 <jian.ma01@hand-china.com>
2020-08-18 17:49:44 +0800

    题解

--- Week_01/ContainerWithMostWater.java
+++ Week_01/ContainerWithMostWater.java
@@ -22,8 +22,9 @@ public class ContainerWithMostWater {
     public int maxArea(int[] height) {
         int max = 0;
         for (int i = 0, j = height.length - 1; i != j; ) {
-            // 每次都移动高度较小的一端
+            // 每次都移动高度较小的一端，返回值是较小的一端高度（类似木桶原理）
             int h = height[i] > height[j] ? height[j--] : height[i++];
+            // 计算面积大小并和之前的值作比较
             max = Math.max(max, (j - i + 1) * h);
         }
         return max;
